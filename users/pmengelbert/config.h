57fadedc0fc17f1be7e5bf16b7b8bbd1e03a1e91
Peter Engelbert <pmengelbert@gmail.com>
2024-04-19 13:03:56 -0400

    save
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

---
+++ users/pmengelbert/config.h
@@ -0,0 +1,12 @@
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 220
+
+#define PERMISSIVE_HOLD
+
+/* #define DUMMY_MOD_NEUTRALIZER_KEYCODE KC_F18 */
+
+/* #define RETRO_TAPPING */
+
+/* // Neutralize left alt and left GUI (Default value) */
+/* #define MODS_TO_NEUTRALIZE { MOD_BIT(KC_LEFT_ALT),  MOD_BIT(KC_LEFT_GUI), MOD_BIT(KC_RIGHT_GUI), MOD_BIT(KC_RIGHT_ALT) } */
+
